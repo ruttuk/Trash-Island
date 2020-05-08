deaeb24d854a4295dcc2d3ed16bdf6e4724730e7
addison <tuf18810@temple.edu>
2020-05-08 14:03:11 -0700

    Revert "Revert "Added Example Island Textures For ReadMe""
    This reverts commit cb5e3df6455f896466016317b7d585b65e725d2f.

--- Assets/Scripts/Island/IslandGenerator.cs
+++ Assets/Scripts/Island/IslandGenerator.cs
@@ -44,8 +44,9 @@ public class IslandGenerator : MonoBehaviour
 
             if(saveTexureToPng)
             {
-                string _fullPath = Application.dataPath + "/Resources/Materials/MenuIslandMat/MenuIslandTexture.png";
-                byte[] _bytes = islandMapTexture.EncodeToPNG();
+                string fileName = editorBiome.name + "_p" + islandMassDataEditor.numPoints + "_s" + islandMassDataEditor.islandSize;
+                string _fullPath = Application.dataPath + "/Resources/Etc/" + fileName +".jpg";
+                byte[] _bytes = islandMapTexture.EncodeToJPG();
                 System.IO.File.WriteAllBytes(_fullPath, _bytes);
                 Debug.Log(_bytes.Length / 1024 + "Kb was saved as: " + _fullPath);
             }
